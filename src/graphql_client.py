e9f52ed89c2096eeea399254282efb7de336445f
vitorlany <vitorlany99@gmail.com>
2024-11-20 18:00:01 -0300

    feat: retry

--- src/graphql_client.py
+++ src/graphql_client.py
@@ -1,15 +1,29 @@
 import requests
+import time
 
-def fetch_graphql(_config, _query):
+def fetch_graphql(_config, _query, max_retries=4, initial_delay=1):
     url = _config['url_graphql']
+    retries = 0
+    delay = initial_delay
+
     headers = {
         'Authorization': _config['authorization']
     }
 
-    response = requests.post(url, json={'query': _query}, headers=headers)
-
-    if response.status_code == 200:
-        return response
-    else:
-        print(f"Query failed to run by returning code of {response.status_code}. {response.text}")
-        return fetch_graphql(_config, _query)
+    while retries < max_retries:
+        response = requests.post(url, json={'query': _query}, headers=headers)
+        if response.status_code == 200:
+            return response
+        elif response.status_code == 429:
+            print(f"Rate limit exceeded. Retrying in {delay} seconds.")
+            time.sleep(delay)
+            delay *= 2
+            retries += 1
+        elif response.status_code in [502, 503]:
+            print(f"Server error ({response.status_code}). Retrying in {delay} seconds.")
+            time.sleep(delay)
+            delay *= 2
+            retries += 1
+        else:
+            print(f"Request failed with status code: {response.status_code} \n {response.json()}")
+            break
--- src/rest_client.py
+++ src/rest_client.py
@@ -1,10 +1,15 @@
 import requests
+import time
 
-def fetch_rest(_config):
+def fetch_rest(_config, max_retries=4, initial_delay=1):
     url = _config['url_rest']
+    retries = 0
+    delay = initial_delay
+
     headers = {
         'Authorization': _config['authorization']
     }
+    
     params = {
         'q': 'stars:>1',
         'sort': 'stars',
@@ -12,10 +17,20 @@ def fetch_rest(_config):
         'per_page': 10
     }
 
-    response = requests.get(url, headers=headers, params=params)
-
-    if response.status_code == 200:
-        return response
-    else:
-        print(f"Query failed to run by returning code of {response.status_code}. {response.text}")
-        return fetch_rest(_config)
+    while retries < max_retries:
+        response = requests.get(url, headers=headers, params=params)
+        if response.status_code == 200:
+            return response
+        elif response.status_code == 429:
+            print(f"Rate limit exceeded. Retrying in {delay} seconds.")
+            time.sleep(delay)
+            delay *= 2
+            retries += 1
+        elif response.status_code in [502, 503]:
+            print(f"Server error ({response.status_code}). Retrying in {delay} seconds.")
+            time.sleep(delay)
+            delay *= 2
+            retries += 1
+        else:
+            print(f"Request failed with status code: {response.status_code} \n {response.json()}")
+            break
